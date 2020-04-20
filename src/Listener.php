e2f661dae76b8f4560bf3d239b9558a85bd0116d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-04-21 00:11:54 +0200

    Extract contracts in it's own package

--- src/Listener.php
+++ src/Listener.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace WyriHaximus\Broadcast\Marker;
+namespace WyriHaximus\Broadcast\Contracts;
 
 interface Listener
 {
--- tests/TestMessage.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace WyriHaximus\Tests\Broadcast;
-
-final class TestMessage
-{
-}
